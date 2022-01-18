3b8e07c37801543680adb00ca7a675f6ee04f0be
nakano ryo <>
2022-01-18 07:59:33 +0000

    kadai11-4

--- create.blade.php
+++ create.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>Mynews</title>
-    </head>
-    <body>
-        <h1>My</h1>
-    </body>
-</html>
+@section('title', 'プロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
